3376ad2c80c9bd0fe957843f272934089548ab3c
treshgame <74305963+treshgame@users.noreply.github.com>
2021-02-14 17:16:46 +0300

    Site for my girl

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// alert('Я тебя очень люблю, моё золото');
+
+$('.single-item').slick({
+	infinite: true,
+	dots: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 2,
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	      }
+	    }
+    ]
+});
